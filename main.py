2eeb82a4954d810c2fda76b034ac068e70e44ebf
meganjchiang <mjchiang@uw.edu>
2023-05-23 09:36:50 -0700

    add 1st data visualization (highest and lowest rated movies)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 # import nltk
+
+sns.set()
 
 
 def merge_and_clean(movies: pd.DataFrame,
@@ -29,7 +33,7 @@ def merge_and_clean(movies: pd.DataFrame,
 
     # add column for updated scores (labels of 1-5)
     movie_reviews['score_category'] = \
-        movie_reviews['review_score'].apply(convert_to_category)
+        movie_reviews['review_score'].apply(convert_to_label)
 
     # return merged and cleaned dataset
     return movie_reviews
@@ -44,19 +48,116 @@ def convert_to_float(fraction: str) -> float:
     return float(numbers[0]) / float(numbers[1])
 
 
-def convert_to_category(score: float) -> str:
+def convert_to_label(score: float) -> int:
+    """
+    Converts a given score (float) to a label (int).
+    """
     if score <= 0.2:
-        return "1"
+        return 1
     elif score <= 0.4:
-        return "2"
+        return 2
     elif score <= 0.6:
-        return "3"
+        return 3
     elif score <= 0.8:
-        return "4"
+        return 4
     else:
-        return "5"
+        return 5
+
 
 # First data visualization
+def plot_top_20_movies(movie_reviews: pd.DataFrame) -> None:
+    # create new plot (otherwise plots will save on top of it)
+    plt.figure()
+
+    # get number of reviews for each movie
+    num_reviews = movie_reviews.groupby('movie_title')['movie_title'].count()
+
+    # filter for only movies with at least 25 reviews
+    at_least_25_reviews = num_reviews[num_reviews >= 25]  # 471 total
+
+    # get movie titles
+    movie_titles = pd.Series(at_least_25_reviews.keys())
+
+    # get rows of movies (from movie_reviews) that arein movie_titles series
+    has_at_least_25_reviews = movie_reviews['movie_title'].isin(movie_titles)
+    movies = movie_reviews[has_at_least_25_reviews]
+
+    # get top 20 movies with highest average ratings
+    avg_scores = movies.groupby('movie_title')['score_category'].mean()
+    top_20_movies = avg_scores.nlargest(20)
+
+    # convert series to dataframe
+    top_20_movies = top_20_movies.reset_index()
+
+    # make bar chart
+    top_20_plot = sns.barplot(data=top_20_movies, x='score_category',
+                              y='movie_title')
+
+    # add avg rating next to each bar
+    # source:
+    # matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.bar_label.html
+    top_20_plot.bar_label(top_20_plot.containers[0], fmt='%.3f',
+                          fontsize=10, fontweight='bold', padding=4)
+
+    # title and axes labels
+    plt.suptitle('Top 20 Highest-Rated Movies* on Rotten Tomatoes (2015-2020)',
+                 fontsize='large', fontweight='bold', x=0.2)
+    plt.title('*For Movies With At Least 30 Reviews',
+              fontsize='small', x=0.46, y=1.02)
+    plt.xlabel("Average Review Score", fontweight='bold')
+    plt.ylabel("Movie", fontweight='bold')
+
+    # save plot
+    plt.savefig('20_highest_rated_movies.png', bbox_inches='tight')
+
+
+def plot_bottom_20_movies(movie_reviews: pd.DataFrame) -> None:
+    # create new plot (otherwise plots will save on top of it)
+    plt.figure()
+
+    # get number of reviews for each movie
+    num_reviews = movie_reviews.groupby('movie_title')['movie_title'].count()
+
+    # filter for only movies with at least 25 reviews
+    at_least_25_reviews = num_reviews[num_reviews >= 25]  # 471 total
+
+    # get movie titles
+    movie_titles = pd.Series(at_least_25_reviews.keys())
+
+    # get rows of movies (from movie_reviews) that arein movie_titles series
+    has_at_least_25_reviews = movie_reviews['movie_title'].isin(movie_titles)
+    movies = movie_reviews[has_at_least_25_reviews]
+
+    # get top 20 movies with lowest average ratings
+    avg_scores = movies.groupby('movie_title')['score_category'].mean()
+    bottom_20_movies = avg_scores.nsmallest(20)
+
+    # convert series to dataframe
+    bottom_20_movies = bottom_20_movies.reset_index()
+
+    # make bar chart
+    bottom_20_plot = sns.barplot(data=bottom_20_movies,
+                                 x='score_category', y='movie_title')
+
+    # add avg rating next to each bar
+    # source:
+    # matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.bar_label.html
+    bottom_20_plot.bar_label(bottom_20_plot.containers[0], fmt='%.3f',
+                             fontsize=10, fontweight='bold', padding=4)
+
+    # title and axes labels
+    plt.suptitle('Top 20 Lowest-Rated Movies* on Rotten Tomatoes (2015-2020)',
+                 fontsize='large', fontweight='bold', x=0.35)
+    plt.title('*For Movies With At Least 30 Reviews',
+              fontsize='small', x=0.65, y=1.02)
+    plt.xlabel("Average Review Score", fontweight='bold')
+    plt.ylabel("Movie", fontweight='bold')
+    plt.xticks(range(6))
+    plt.xlim(0, 5)
+
+    # save plot
+    plt.savefig('20_lowest_rated_movies.png', bbox_inches='tight')
+
 # Second data visualization
 # Third data visualization
 
@@ -67,6 +168,10 @@ def main():
 
     # join 2 datasets and clean
     movie_reviews = merge_and_clean(movies, reviews)
+
+    # first data visualization (top and bottom 20 movies)
+    plot_top_20_movies(movie_reviews)
+    plot_bottom_20_movies(movie_reviews)
 
     # to do:
     # remove rows with null review scores -> DONE
