21585ae2d22baa21d7fc3429b962d63466db51ad
michelleejkim <mkim45@uw.edu>
2023-05-25 16:53:20 -0700

    add pos and negative worldclouds

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 # import nltk
+import string
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
 
 sns.set()
 
@@ -159,6 +164,130 @@ def plot_bottom_20_movies(movie_reviews: pd.DataFrame) -> None:
     plt.savefig('20_lowest_rated_movies.png', bbox_inches='tight')
 
 # Second data visualization
+def wordcloud_positive(movie_reviews: pd.DataFrame) -> None:
+
+    # get number of reviews for each movie
+    num_reviews = movie_reviews.groupby('movie_title')['movie_title'].count()
+
+    # filter for only movies with at least 25 reviews
+    at_least_25_reviews = num_reviews[num_reviews >= 25]  # 471 total
+    print(at_least_25_reviews)
+
+    # get movie titles
+    movie_titles = pd.Series(at_least_25_reviews.keys())
+
+    # get rows of movies (from movie_reviews) that arein movie_titles series
+    has_at_least_25_reviews = movie_reviews['movie_title'].isin(movie_titles)
+    movies = movie_reviews[has_at_least_25_reviews]
+
+    # get top 20 movies with highest average ratings
+    avg_scores = movies.groupby('movie_title')['score_category'].mean()
+    top_20_movies = avg_scores.nlargest(20)
+
+    # convert series to dataframe
+    top_20_movies = top_20_movies.reset_index()
+
+    # getting the top 20 movies with its columns 
+    top_20_movie_reviews = movie_reviews[movie_reviews['movie_title'].isin(top_20_movies['movie_title'])]
+    top_20_movie_reviews = top_20_movie_reviews.reset_index()
+    # top_20_movie_reviews.to_csv('testttt.csv')
+
+    # creating subset of positive and negative reviews
+    positive_reviews = movie_reviews[movie_reviews['score_category'] > 3]
+
+    # Get the set of stopwords
+    stopwords_set = set(stopwords.words('english'))
+
+    # Get the set of punctuations 
+    punctuation_set = set(string.punctuation)
+
+    # Additional punctuations 
+    add_punc = ["'s", "'nt", "n't"]
+
+    # Remove stopwords and punctuations
+    positive_words = []
+    for review in positive_reviews['review_content']:
+        words = word_tokenize(review)
+        cleaned_words = [word.lower() for word in words if word.lower() not in stopwords_set and word not in punctuation_set and not all(char in string.punctuation for char in word) and word not in add_punc]
+        positive_words.extend(cleaned_words)
+
+    # Find the top 10 most common words
+    positive_freq = FreqDist(positive_words)
+
+    # Convert the word frequency distribution to a string
+    positive_freq_txt = ' '.join(positive_freq.keys())
+
+    # Create word cloud for positive subset
+    positive_wordcloud = WordCloud(width=1000, height=1000, background_color='white').generate(positive_freq_txt)
+    plt.figure(figsize=(12, 12))
+    plt.imshow(positive_wordcloud, interpolation="bilinear")
+    plt.axis('off')
+    plt.savefig('positive_wordcloud.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+
+def wordcloud_negative(movie_reviews: pd.DataFrame) -> None:
+
+    # get number of reviews for each movie
+    num_reviews = movie_reviews.groupby('movie_title')['movie_title'].count()
+
+    # filter for only movies with at least 25 reviews
+    at_least_25_reviews = num_reviews[num_reviews >= 25]  # 471 total
+    print(at_least_25_reviews)
+   
+    # get movie titles
+    movie_titles = pd.Series(at_least_25_reviews.keys())
+
+    # get rows of movies (from movie_reviews) that are in movie_titles series
+    has_at_least_25_reviews = movie_reviews['movie_title'].isin(movie_titles)
+    movies = movie_reviews[has_at_least_25_reviews]
+    #print(movies)
+
+    # get top 20 movies with highest average ratings
+    avg_scores = movies.groupby('movie_title')['score_category'].mean()
+    top_20_movies = avg_scores.nlargest(20)
+
+    # convert series to dataframe
+    top_20_movies = top_20_movies.reset_index()
+
+    # getting the top 20 movies with its columns 
+    top_20_movie_reviews = movie_reviews[movie_reviews['movie_title'].isin(top_20_movies['movie_title'])]
+    top_20_movie_reviews = top_20_movie_reviews.reset_index()
+    # top_20_movie_reviews.to_csv('testttt.csv')
+
+    # creating subset of negative reviews
+    negative_reviews = movie_reviews[movie_reviews['score_category'] < 3]
+
+    # Get the set of stopwords
+    stopwords_set = set(stopwords.words('english'))
+
+    # Get the set of punctuations 
+    punctuation_set = set(string.punctuation)
+
+    # additional punctuations 
+    add_punc = ["'s", "'nt", "n't"]
+    
+    # remove stopwords and punctuations for negative reviews
+    negative_words = []
+    for review in negative_reviews['review_content']:
+        words = word_tokenize(review)
+        cleaned_words = [word.lower() for word in words if word.lower() not in stopwords_set and word not in punctuation_set and not all(char in string.punctuation for char in word) and word not in add_punc]
+        negative_words.extend(cleaned_words)
+
+    # Find the top 10 most common words
+    negative_freq = FreqDist(negative_words)
+
+    # Convert the word frequency distribution to a string
+    negative_freq_txt = ' '.join(negative_freq.keys())
+
+    # Create word cloud for negative subset
+    negative_wordcloud = WordCloud(width=1000, height=1000, background_color='white').generate(negative_freq_txt)
+    plt.figure(figsize=(12, 12))
+    plt.imshow(negative_wordcloud, interpolation="bilinear")
+    plt.axis('off')
+    plt.savefig('negative_wordcloud.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
 # Third data visualization
 
 
@@ -172,6 +301,9 @@ def main():
     # first data visualization (top and bottom 20 movies)
     plot_top_20_movies(movie_reviews)
     plot_bottom_20_movies(movie_reviews)
+    wordcloud_positive(movie_reviews)
+    wordcloud_negative(movie_reviews)
+
 
     # to do:
     # remove rows with null review scores -> DONE
